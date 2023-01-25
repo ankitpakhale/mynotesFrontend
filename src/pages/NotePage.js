5238c69a99426d2ff07054f9d47f9f57b68b06fa
Ankit Pakhale <akp3067@gmail.com>
2023-01-25 15:38:29 +0530

    Added: update api

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = () => {
+  const navigate = useNavigate();
+
   const { id } = useParams();
   const [note, setNote] = useState(null);
 
@@ -17,16 +19,32 @@ const NotePage = () => {
     getNote();
   }, [id]);
 
+  const updateNote = async () => {
+    fetch(`/api/notes/${note.id}/update/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  const handleSubmit = () => {
+    updateNote();
+    navigate("/");
+  };
+
   return (
     <div className="note">
       <div className="note-header">
         <h3>
-          <Link to={"/"}>
-            <ArrowLeft />
-          </Link>
+          <ArrowLeft onClick={() => handleSubmit()} />
         </h3>
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={(e) => setNote({ ...note, body: e.target.value })}
+        defaultValue={note?.body}
+      ></textarea>
     </div>
   );
 };
