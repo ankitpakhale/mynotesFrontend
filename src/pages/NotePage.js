e0c292b9779a86fe0c564aec2b42fd73250a5580
Ankit Pakhale <akp3067@gmail.com>
2023-01-25 16:33:27 +0530

    Added: post api and floating button

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -9,6 +9,8 @@ const NotePage = () => {
   const [note, setNote] = useState(null);
 
   const getNote = async () => {
+    if (id === "new") return;
+
     let response = await fetch(`/api/notes/${id}/`);
     let data = await response.json();
     console.info(data, "this is data");
@@ -29,8 +31,37 @@ const NotePage = () => {
     });
   };
 
+  const addNote = async () => {
+    if (note) {
+      fetch("/api/post/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(note),
+      });
+      console.info("new note added successfully");
+      navigate("/");
+    }
+    console.info("Please add something to notes");
+  };
+
   const handleSubmit = () => {
-    updateNote();
+    if (id !== "new") {
+      console.info("new note updated successfully");
+      updateNote();
+    }
+    navigate("/");
+  };
+
+  const deleteNote = async () => {
+    fetch(`/api/notes/${note.id}/delete/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.info("data deleted successfully");
     navigate("/");
   };
 
@@ -40,7 +71,14 @@ const NotePage = () => {
         <h3>
           <ArrowLeft onClick={() => handleSubmit()} />
         </h3>
+
+        {id !== "new" ? (
+          <button onClick={() => deleteNote()}>Delete</button>
+        ) : (
+          <button onClick={() => addNote()}>Done</button>
+        )}
       </div>
+      <button onClick={() => console.info(note)}>Show data</button>
       <textarea
         onChange={(e) => setNote({ ...note, body: e.target.value })}
         defaultValue={note?.body}
