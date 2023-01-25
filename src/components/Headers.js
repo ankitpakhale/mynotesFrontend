283d456d5dd56247d6e7f9cef70aee370f56f93c
Ankit Pakhale <akp3067@gmail.com>
2023-01-25 12:58:27 +0530

    Added: Router

---
+++ src/components/Headers.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Headers = () => {
+  return (
+    <div>
+      <h1>Our headers</h1>
+    </div>
+  );
+};
+export default Headers;
---
+++ src/pages/NotesListPage.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import ListItem from "../components/ListItem";
+
+const NotesListPage = () => {
+  let [notes, setNotes] = useState([]);
+
+  let getNotes = async () => {
+    let response = await fetch("/api/notes/");
+    let data = await response.json();
+    setNotes(data);
+  };
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  return (
+    <div>
+      <div className="notes-list">
+        {notes.map((note, index) => (
+          <ListItem key={index} note={note} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NotesListPage;
